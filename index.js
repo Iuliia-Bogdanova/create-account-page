ccbd4b388ade4b14703fba07baed2f1e72362ddb
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-10-16 12:06:49 +0400

    changed on input

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function showMessage() {
+/*function showMessage() {
     let name = prompt('What is your name?', "");
     
     alert(`Hello ${name} !`);
+}*/
+
+let showMessage = () => {
+    let name = prompt('What is your name?', "");
+   
+    alert(`Hello ${name} !`);
 }
 
-showMessage();
+//console.log(`Hello ${name} !`);
